b211c5cfe9fde0a622a0f8da19bc6d8dfdfa91a5
Brian Fitton <bfitton@nyit.edu>
2021-02-02 23:46:00 -0500

    added new mythos form

--- config/formats.ts
+++ config/formats.ts
@@ -493,7 +493,7 @@ export const Formats: FormatList = [
 		mod: 'gen8',
 		ruleset: ['Standard NatDex'],
 	},
-		{
+	{
 		name: "[Gen 8] National Dex Triples",
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/3533914/">ORAS Battle Spot Triples Discussion</a>`,
